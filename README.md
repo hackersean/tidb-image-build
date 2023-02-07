82865bcd0a6814f86984c165bac411194022e340
sean.liu <sean.liu@pingcap.com>
2023-02-07 15:19:11 +0800

    base function completed

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # 功能
-build & push 指定或所有镜像
+build  指定的镜像
 
-# 流程
-1. 初始化 build 环境，目录。
-2. 编译 Dockerfile 模板
+## 用法
+1. 先根据用户环境，手动制作一个基础镜像。
+2. 将安装文件解压放到一个目录中。多文件快速解压命令见备注。
+3. 运行工具，制作镜像。工具会自动build和检查。最终输出push命令。如果制作失败，会输出失败列表。
+4. 复制输出的命令执行push镜像即可。
+
+## 工具流程
+工具会做如下操作
+1. 编译 Dockerfile 模板
+2. 初始化 build 目录,解析模板中需要COPY的文件到 build 目录。
 3. build
-4. push
+4. 检查镜像
+5. 输出push命令
+
+# 操作
+## 解压tidb企业版安装包
+将企业服务安装包的镜像解压到指定目录。 这个目录对于参数的 --mirror-dir
+```bash
+ls tidb-enterprise-server-v6.1.1-linux-amd64/*.tar.gz | xargs -P4 -i  tar xzvf '{}' -C mirror
+```
+## 运行命令
+```bash
+python build.py --mirror-dir ~/tmp/build/mirror/ --work-path ~/tmp/worker/ --dockerfile-template dockerfile/tikv --base-image gcr.io/pingcap-public/pingcap/alpine-glibc:alpine-3.14.3 --image-namespace "pingcap.com/test" --image-version v6.1.1 --log-file=/tmp/build.log
+```
+
+# 备注
+## 注意
+1. 历史镜像制作的遗留问题，lightning镜像中会有br工具，请注意。
+2. 如用户有特殊配置，比如yum repo等，请先制作base镜像，再以base镜像作为基础制作组件镜像。
+
+## 清理重复镜像
+如果重复build同tag名的镜像，新的镜像会把旧的镜像取代。如果制作镜像的机器空间满了，可以清理下。
+``` bash
+docker rmi `docker images|grep none|awk '{print $3}'`
+```
--- build.py
+++ build.py
@@ -8,89 +8,193 @@ import subprocess
 import logging
 
 TAR_CMD = "tar -zxvf {tar_path} -C {out_path}"
+base_image = None
+mirror_path = None
+work_path = None
+image_namespace = None
+image_version = None
 
 
 def command(cmd):
-    logging.debug("command:[%s]", cmd)
+    logging.info("command:[%s]", cmd)
     result = subprocess.getstatusoutput(cmd)
     return result
 
 
-def decompression_total_binary_packet(tar_path, out_path):
-    os.makedirs(out_path, exist_ok=True)
-    code, output = command(TAR_CMD.format(tar_path=tar_path,
-                                          out_path=out_path))
-    if code != 0:
-        raise BaseException("decompression failed", code, output)
+def parser_dockerfile(template_path, dockerfile_path, base_image):
+    rf = open(template_path)
+    lines = rf.read()
+    rf.close()
+
+    wf = open(dockerfile_path, "w+")
+    wf.write("FROM %s\n" % (base_image,))  # add from
+    wf.write(lines)
+    wf.write("\n")
+    wf.close()
     return
 
 
-def make_build_dir(component_lsit, build_path):
-    for filename in component_lsit:
-        # print build_path,filename
-        subpath = os.path.join(build_path, "images", filename)
-        # print(subpath)
-        os.makedirs(subpath, exist_ok=True)
+def build_image(image_name, dockerfile_path, build_path):
+    CMD_Template = 'docker build --no-cache --tag="{image_name}" -f {dockerfile_path} {build_path}'
+    cmd = CMD_Template.format(
+        image_name=image_name, dockerfile_path=dockerfile_path, build_path=build_path)
+    logging.info("build image %s", image_name)
+    returnCode, output = command(cmd)
+    if returnCode != 0:
+        logging.critical(output)
+        raise RuntimeError("build failed: %s" % output)
 
-
-def decompression_every_binary_packet(component_lsit, tar_path, build_path):
-    _, tar_filename = os.path.split(tar_path)
-    tar_dir = os.path.join(build_path,
-                           tar_filename.split(".tar.gz")[0])
-    for file in os.listdir(tar_dir):
-        if file.endswith(".tar.gz"):
-            print(file.split("-v")[0])
-    print("\n")
-    for name in component_lsit:
-        print(name)
+    logging.info("build image log success: %s", output)
+    return
 
 
 def init():
     parser = argparse.ArgumentParser(
         description='build tidb enterprise server images.')
-    parser.add_argument('--tidb-binary-packet',
+    parser.add_argument('--mirror-dir',
                         required=True,
                         type=str,
-                        help='tidb binary packet like *.tar.gz')
-    parser.add_argument('--build-tmp-path',
+                        help='please Extract all files to this path')
+    parser.add_argument('--work-path',
                         required=True,
                         type=str,
-                        default="/tmp/build",
-                        help='tmp path of build docker images')
-    parser.add_argument('--docker-file-path',
+                        default="/tmp/work",
+                        help='work path of build docker images')
+    parser.add_argument('--dockerfile-template',
                         required=True,
                         type=str,
-                        help='docker file path')
+                        nargs="+",
+                        help='docker file template path')
     parser.add_argument('--base-image',
                         required=True,
                         type=str,
                         help='base image name')
+    parser.add_argument('--image-namespace',
+                        required=True,
+                        type=str,
+                        help='base image namepsace')
+    parser.add_argument('--image-version',
+                        required=True,
+                        type=str,
+                        help='base image version')
+
+    parser.add_argument('--log-file',
+                        required=False,
+                        type=str,
+                        help='log file name,default is stdout')
 
     args = parser.parse_args()
     return args
 
 
+def prepare_dockerfile(dockerfile_template_path, filename, work_path):
+
+    os.makedirs(os.path.join(work_path, "dockerfile"), exist_ok=True)
+
+    output_path = os.path.join(work_path, "dockerfile", filename)
+    parser_dockerfile(dockerfile_template_path, output_path, base_image)
+
+    return output_path
+
+# copy file to images
+
+
+def prepare_build_file(dockerfile_path, mirror_path, build_path):
+    rf = open(dockerfile_path)
+    lines = rf.readlines()
+    rf.close()
+
+    cmd_list = []
+    for line in lines:
+        tmp_list = line.split()
+        if len(tmp_list) < 3:
+            continue
+
+        if tmp_list[0] == "COPY":
+            cmd_list.append("cd %s && cp %s %s" %
+                            (mirror_path, "".join(tmp_list[1:-1]), build_path))
+
+    os.makedirs(build_path, exist_ok=True)
+    for cmd in cmd_list:
+        (returnCode, output) = command(cmd)
+        if returnCode != 0:
+            logging.critical(output)
+            raise RuntimeError(
+                "prepare failed: %s" % output)
+    return
+
+
+def check_image(component, image_name):
+    successReturnCodes = {}
+    CMD_TMP = "docker run -it --rm {image_name} -h".format(
+        image_name=image_name)
+    (returnCode, output) = command(CMD_TMP)
+    if returnCode != 0:
+        raise RuntimeError("check error: %s" % output)
+    return
+
+
+def build(dockerfile_template_path):
+    (_, filename) = os.path.split(dockerfile_template_path)
+    image_name = image_namespace+"/"+filename+":"+image_version
+
+    logging.info("build %s start", image_name)
+
+    dockerfile_path = prepare_dockerfile(
+        dockerfile_template_path, filename, work_path)
+
+    build_path = os.path.join(work_path, "build", filename)
+    prepare_build_file(
+        dockerfile_path, mirror_path, build_path)
+
+    build_image(image_name, dockerfile_path, build_path)
+
+    check_image(filename, image_name)
+    return image_name
+
+
 def main():
+    global base_image
+    global mirror_path
+    global work_path
+    global image_namespace
+    global image_version
+
     args = init()
     # print(args)
-    tar_path = args.tidb_binary_packet
-    build_tmp_path = args.build_tmp_path
-    try:
-        component_lsit = os.listdir(args.docker_file_path)
-        logging.info("decompression %s", tar_path)
-        # decompression_total_binary_packet(tar_path, build_tmp_path)
-        make_build_dir(component_lsit, build_tmp_path)
-        logging.info("complete decompression %s", tar_path)
-        decompression_every_binary_packet(
-            component_lsit, tar_path, build_tmp_path)
-    except BaseException:
-        raise
+    base_image = args.base_image
+    mirror_path = args.mirror_dir
+    work_path = args.work_path
+    dockerfile_template_list = args.dockerfile_template
+
+    image_namespace = args.image_namespace
+    image_version = args.image_version
+
+    LOG_FORMAT = "%(asctime)s - %(pathname)s - [%(lineno)d] - %(levelname)s - %(message)s"
+    DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
+    logging.basicConfig(level=logging.DEBUG,
+                        format=LOG_FORMAT,
+                        datefmt=DATE_FORMAT,
+                        filename=args.log_file,
+                        filemode="w")
+
+    error_list = []
+    for dockerfile_template_path in dockerfile_template_list:
+        try:
+            print("# build image for %s start,please wait" %
+                  (dockerfile_template_path,))
+            image_name = build(dockerfile_template_path)
+            print("# image %s build success,and check passed,you can push" %
+                  (image_name))
+            print("docker push %s" % image_name)
+        except Exception as e:
+            print("# build failed,%s,details in log,[%s]" %
+                  (e, args.log_file))
+            error_list.append(dockerfile_template_path)
+
+    if len(error_list) > 0:
+        print("# error list [%s]" % (" ".join(error_list),))
 
 
 if __name__ == "__main__":
-    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-    DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
-    logging.basicConfig(level=logging.DEBUG,
-                        format=LOG_FORMAT, datefmt=DATE_FORMAT)
-
     main()
